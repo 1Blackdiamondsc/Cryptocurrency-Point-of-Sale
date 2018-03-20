7449892e63b19a2ade51cf0e38d4d5e904c5e82b
stephenfriedman <friedmansemail@gmail.com>
2018-03-20 01:03:11 -0400

    View 100 most recent transactions to/from a single public key

--- index.js
+++ index.js
@@ -8,21 +8,36 @@
 
 'use strict';
 const RippleAPI = require('ripple-lib').RippleAPI;
-
+const myAddress = '';
+const transactionCount = 100;
 const api = new RippleAPI({
-  server: 'wss://s1.ripple.com' // Public ripple server
+  server: 'wss://s2.ripple.com' // Public ripple server
 });
+
 api.connect().then(() => {
-  console.log("connected to a ripple server");
   console.log("Server info: ",api.getServerInfo());
-  const myAddress = '';
   console.log('getting account info for', myAddress);
   return api.getAccountInfo(myAddress);
-
 }).then(info => {
   console.log(info);
-  console.log('getAccountInfo finished');
-}).then(() => {
+  console.log("-----------------------------------------------------------------------------------------");
+  return api.getTransactions(myAddress, {limit: transactionCount})
+}).then(transactionHistory => {
+  console.log("TRANSACTION HISTORY LAST ",transactionCount);
+  for(var tx in transactionHistory){
+    let currTx = transactionHistory[tx];
+    if(currTx.outcome && currTx.outcome.result  && currTx.outcome.result === 'tesSUCCESS'){
+      if(currTx.outcome.deliveredAmount && currTx.outcome.fee)
+      {
+        console.log("FROM:  ",currTx.specification.source.address);
+        console.log("TO:    ",currTx.specification.destination.address);
+        console.log("WHEN:  ",currTx.outcome.timestamp);         
+        console.log("AMOUNT: Ʀ",currTx.outcome.deliveredAmount.value); 
+        console.log("FEE:   ",currTx.outcome.fee)       
+        console.log("-----------------------------------------------------------------------------------------");
+      }
+    }
+  }
   return api.disconnect();
 }).then(() => {
   console.log('done and disconnected.');
