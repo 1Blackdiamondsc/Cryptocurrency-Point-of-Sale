6cc7a56a98e6ef1406847d056c646c3d22635501
stephenfriedman <friedmansemail@gmail.com>
2018-03-23 21:25:05 -0400

    Check for incoming XRP every 7 seconds, 50 times

--- index.js
+++ index.js
@@ -8,39 +8,81 @@
 
 'use strict';
 const RippleAPI = require('ripple-lib').RippleAPI;
-const myAddress = '';
-const transactionCount = 100;
+//Kraken exchange wallet, has many incoming transactions, good for demo
+const myAddress = 'rLHzPsX6oXkzU2qL12kHCH8G8cnZv1rBJh';
+const transactionCount = 3;
 const api = new RippleAPI({
   server: 'wss://s2.ripple.com' // Public ripple server
 });
+var recievedTxns= [];
+const startRunTimes=50;
+var runTimes=startRunTimes;
+var timer = setInterval(function(){
+  if(runTimes>0){
+    rippleUpdate()
+  }
+  else{
+    clearInterval(timer);
+  }}, 7000);
 
-api.connect().then(() => {
-  console.log("Server info: ",api.getServerInfo());
-  console.log('getting account info for', myAddress);
-  return api.getAccountInfo(myAddress);
-}).then(info => {
-  console.log(info);
-  console.log("-----------------------------------------------------------------------------------------");
-  return api.getTransactions(myAddress, {limit: transactionCount})
-}).then(transactionHistory => {
-  console.log("TRANSACTION HISTORY LAST ",transactionCount);
-  for(var tx in transactionHistory){
-    let currTx = transactionHistory[tx];
-    if(currTx.outcome && currTx.outcome.result  && currTx.outcome.result === 'tesSUCCESS'){
-      if(currTx.outcome.deliveredAmount && currTx.outcome.fee)
-      {
-        console.log("FROM:  ",currTx.specification.source.address);
-        console.log("TO:    ",currTx.specification.destination.address);
-        console.log("WHEN:  ",currTx.outcome.timestamp);         
-        console.log("AMOUNT: Ʀ",currTx.outcome.deliveredAmount.value); 
-        console.log("FEE:   ",currTx.outcome.fee)       
-        console.log("-----------------------------------------------------------------------------------------");
+
+function rippleUpdate()
+{
+  if(runTimes===startRunTimes){
+    console.log("TRANSACTION HISTORY LAST ",transactionCount);
+  }
+  
+  api.connect().then(() => {
+    if(runTimes===startRunTimes){  
+      console.log("Server info: ",api.getServerInfo());
+      console.log('getting account info for', myAddress);
+    }
+    return api.getAccountInfo(myAddress);
+  }).then(info => {
+    if(runTimes===startRunTimes){
+      console.log(info);
+      console.log("-----------------------------------------------------------------------------------------");
+    }
+    return api.getTransactions(myAddress, {limit: transactionCount})
+  }).then(transactionHistory => {
+    let newTxn = false;
+    for(var tx in transactionHistory){
+      let currTx = transactionHistory[tx];
+      if(currTx.outcome && currTx.outcome.result  && currTx.outcome.result === 'tesSUCCESS'){
+        //Only check for incoming transactions where XRP is incoming to myAddress
+        if(currTx.outcome.deliveredAmount && currTx.outcome.fee && currTx.specification.destination.address === myAddress)
+        {
+          if(!recievedTxns.includes(currTx.outcome.timestamp+currTx.specification.source.address))
+          {
+            newTxn=true;
+            recievedTxns.push(currTx.outcome.timestamp+currTx.specification.source.address);
+            console.log("FROM:  ",currTx.specification.source.address);
+            console.log("WHEN:  ",currTx.outcome.timestamp);         
+            console.log("AMOUNT: Ʀ",currTx.outcome.deliveredAmount.value); 
+            console.log("FEE:   ",currTx.outcome.fee)       
+            console.log("-----------------------------------------------------------------------------------------");
+          }          
+        }
       }
     }
-  }
-  return api.disconnect();
-}).then(() => {
-  console.log('done and disconnected.');
-}).catch(console.error);
-
+    runTimes = runTimes-1;
+    if(!newTxn){
+      console.log("NO NEW INCOMING TRANSACTIONS")
+      console.log("-----------------------------------------------------------------------------------------");                
+    }
+    if(runTimes<=0){
+      return api.disconnect();
+    }
+    else{
+      return;
+    }
+  }).then(() => {
+    if(runTimes<=0){
+      console.log('done and disconnected.');
+    }
+    else{
+      console.log("Still running: ",runTimes," refreshes left");
+    }
+  }).catch(console.error);
+}
 //hellooooo crypto
