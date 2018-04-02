4c32839311fb1bbb07adcd20e0e675e16616e001
stephen.friedman <friedmansemail@gmail.com>
2018-04-02 01:26:59 -0400

    Get most recent IOTA transaction from an address

---
+++ IOTA.js
@@ -0,0 +1,20 @@
+'use strict';
+const IOTA = require('iota.lib.js');
+
+var iota = new IOTA({
+    'provider': 'http://iotanode.host:14265'
+});
+
+//sample address: TAQWMPGBTRMSMX9BBKITOWNXFTFPTPVXYBYDVLUSCRMRWJPJLOOFXUDQVMUGJYMTRTCY9TKLXKBMBOXG9DYMQADCU9
+
+var myAddress = ''
+var searchValues = {
+    'addresses' : [myAddress]
+}
+
+iota.api.findTransactionObjects(searchValues, function(a,b,c){
+    console.log("\n--------------------------------------------------------------------------------------")
+    console.log("Most recent IOTA transaction for address: ",myAddress);
+    console.log("--------------------------------------------------------------------------------------\n")
+    console.log("From:  ",b[0].address.substring(0,10),"...\nValue: ",b[0].value/1000000, "Mi")
+});
